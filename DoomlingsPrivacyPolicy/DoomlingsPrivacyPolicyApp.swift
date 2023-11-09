e9789ee151ae172f79c7ccae70ac6fc024dea208
Zach Uptin <zachary.uptin@me.com>
2023-11-09 16:59:37 +1000

    Initial Commit

---
+++ DoomlingsPrivacyPolicy/DoomlingsPrivacyPolicyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoomlingsPrivacyPolicyApp.swift
+//  DoomlingsPrivacyPolicy
+//
+//  Created by Zach Uptin on 9/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DoomlingsPrivacyPolicyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
